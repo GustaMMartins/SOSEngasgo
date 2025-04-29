070ace52a0617efeefa43acd0b94919c80abd090
thibastos0 <thibastos@live.com>
2025-04-29 11:54:21 -0300

    enviando mensagem para o bot versão 0

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,35 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Telegram Bot Route
+    Route::get('/send-message', function () {
+        $chatId = env('CHAT_ID_TELEGRAM', 'erro'); // Get chat ID from .env file
+        $message = "Olá mundo! Mensagem via Laravel com id $chatId!"; 
+        // ID Bot = 7807867662
+    
+        Telegram::sendMessage([
+            'chat_id' => $chatId,
+            'text' => $message,
+        ]);
+    
+        return 'Message sent to Telegram!';
+    });
+/*
+    Route::get('/get-updates', function () {
+        $updates = Telegram::getUpdates();
+        return $updates; 
+    });
+
+    Route::get('/set-webhook', function () {
+        $url = env('TELEGRAM_WEBHOOK_URL');
+        $response = Telegram::setWebhook(['url' => $url]);
+        return $response;
+    });
+    Route::get('/delete-webhook', function () {
+        $response = Telegram::deleteWebhook();
+        return $response;
+    });*/
 });
 
 require __DIR__.'/auth.php';
