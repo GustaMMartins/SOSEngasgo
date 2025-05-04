0c5ed8196bb659c7c32c7dff911e030e7fb667a7
thibastos0 <thibastos@live.com>
2025-05-04 20:56:12 -0300

    text white retirado por causa do render teste 1

--- resources/views/telegram/aguardando.blade.php
+++ resources/views/telegram/aguardando.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div class="py-8 bg-blue-900 flex items-center justify-center mt-6 sm:mt-0">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-blue-500/30 backdrop-blur-md border text-white border-white/20 rounded-lg shadow-lg p-8 text-center">
+            <div class="bg-blue-500/30 backdrop-blur-md border border-white/20 rounded-lg shadow-lg p-8 text-center">
                 <h2>Aguardando confirmação da equipe...</h2>
                 <p>ID do atendimento: {{ $atendimento->id }}</p>
                 <p>Status: {{ $atendimento->status }}</p>
